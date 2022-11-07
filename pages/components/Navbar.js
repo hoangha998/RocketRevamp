a9fb2f9d2da7c5751bfa502a9a29efed99d002fc
Moyesh Khanal <Moyeshkhanal@gmail.com>
2022-11-06 19:18:52 -0600

    Added some style and tabs

---
+++ pages/components/Navbar.js
@@ -0,0 +1,96 @@
+import { ReactNode } from "react";
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Link,
+  IconButton,
+  Image,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorMode,
+  useColorModeValue,
+  Stack,
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon, AddIcon } from "@chakra-ui/icons";
+import { BsSun, BsMoonStarsFill } from 'react-icons/bs';
+
+const Links = ["Challenger Learning Center"];
+
+const NavLink = ({ children }, { children: ReactNode }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={"md"}
+    _hover={{
+      textDecoration: "none",
+      bg: useColorModeValue("gray.200", "gray.700"),
+    }}
+    href={"#"}
+  >
+    {children}
+  </Link>
+);
+
+export default function Navbar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const {colorMode, toggleColorMode } = useColorMode();
+  const formBackground = useColorModeValue("gray.100", "gray.700");
+  const LOGO = 'https://www.challengertlh.com/wp-content/uploads/2015/09/siteicon.png';
+  return (
+    <>
+      <Box bg={useColorModeValue("gray.100", "gray.700")} px={4} width="100%">
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={"center"}>
+            <Box>
+              <Image height={50} width={50} src={LOGO}  objectFit={'cover'}/>
+            </Box>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+          </HStack>
+          <Flex alignItems={"center"}>
+            <Button
+              aria-label="Toggle Color Mode"
+              onClick={toggleColorMode}
+              _focus={{ boxShadow: "none" }}
+              w="fit-content"
+            >
+              {colorMode === "light" ? <BsMoonStarsFill /> : <BsSun />}
+            </Button>
+          
+          </Flex>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,102 @@
-import {Flex, Heading, Input,
-   Button, Grid, GridItem,
-   useColorMode, useColorModeValue,
-   Wrap, WrapItem, Box, Progress
- }
-from '@chakra-ui/react'
-import ItemCard from './components/ItemCard'
+import {
+  Flex,
+  Heading,
+  Input,
+  Button,
+  Grid,
+  GridItem,
+  useColorMode,
+  useColorModeValue,
+  Wrap,
+  WrapItem,
+  Box,
+  Progress,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+} from "@chakra-ui/react";
+import ItemCard from "./components/ItemCard";
+import Navbar from "./components/Navbar";
 export default function Home() {
-  const {toggleColorMode} = useColorMode()
-  const formBackground = useColorModeValue("gray.100", "gray.700")
+  const { toggleColorMode } = useColorMode();
+  const formBackground = useColorModeValue("gray.100", "gray.700");
   return (
     <Flex>
-      <Box width="70%">
-        <Heading as="h2" noOfLines={1}> Adhesive </Heading>
-        <Wrap>
-          <WrapItem>
-            <ItemCard> </ItemCard>
-          </WrapItem>
-          <WrapItem>
-            <ItemCard> </ItemCard>
-          </WrapItem>
-          <WrapItem>
-            <ItemCard> </ItemCard>
-          </WrapItem>
-          <WrapItem>
-            <ItemCard> </ItemCard>
-          </WrapItem>
-          <WrapItem>
-            <ItemCard> </ItemCard>
-          </WrapItem>
-          <WrapItem>
-            <ItemCard> </ItemCard>
-          </WrapItem>
-        </Wrap>
-      </Box>
+      <Box width="100%">
+        <Navbar />
 
-      <Box width="30%">
-        <Heading as="h2" fontSize="20px" fontWeight="1"> Your budget </Heading>
-        <Progress colorScheme='green' height="30px" value={80}  rounded="full"/>
+        <Grid templateColumns="repeat(5, 1fr)" gap={4}>
+          <GridItem colSpan={3}>
+            <Box width="100%" margin="10">
+              <Tabs variant="enclosed">
+                <TabList>
+                  <Tab>Adhesive</Tab>
+                  <Tab>Two</Tab>
+                </TabList>
+                <TabPanels>
+                  <TabPanel>
+                    <Box>
+                      <Heading as="h2" noOfLines={1}>
+                        {" "}
+                        Adhesive{" "}
+                      </Heading>
+                      <Wrap>
+                        <WrapItem>
+                          <ItemCard> </ItemCard>
+                        </WrapItem>
+                        <WrapItem>
+                          <ItemCard> </ItemCard>
+                        </WrapItem>
+                        <WrapItem>
+                          <ItemCard> </ItemCard>
+                        </WrapItem>
+                        <WrapItem>
+                          <ItemCard> </ItemCard>
+                        </WrapItem>
+                        <WrapItem>
+                          <ItemCard> </ItemCard>
+                        </WrapItem>
+                        <WrapItem>
+                          <ItemCard> </ItemCard>
+                        </WrapItem>
+                      </Wrap>
+                    </Box>
+                  </TabPanel>
+                  <TabPanel>
+                    <p>two!</p>
+                  </TabPanel>
+                </TabPanels>
+              </Tabs>
+            </Box>
+          </GridItem>
+          <GridItem colStart={4} colEnd={6}>
+            <Box
+              width="30%"
+              padding="5"
+              float="right"
+              position="absolute"
+              top="20"
+              right="50"
+            >
+              <Box width="70%" margin="auto">
+                <Heading as="h2" fontSize="20px" fontWeight="1" padding="5">
+                  {" "}
+                  Your budget{" "}
+                </Heading>
+                <Progress
+                  colorScheme="green"
+                  height="30px"
+                  value={50}
+                  rounded="full"
+                />
+              </Box>
+              <Heading as="h2">Cart</Heading>
+            </Box>
+          </GridItem>
+        </Grid>
       </Box>
     </Flex>
-  )
+  );
 }
